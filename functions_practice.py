a2a00d37c65fb7b350bf0b50f0d748bd69451bd8
Nathan Towe <nathanwtowe89@gmail.com>
2024-02-10 14:53:53 -0500

    finished eat_lunch function

--- functions_practice.py
+++ functions_practice.py
@@ -10,3 +10,19 @@ print(pack(4,2,8))
 
 empty_lunch = []
 lunch_box = ["PBJ", "chips", "fruitcup"]
+
+#2c
+def eat_lunch(lunch):
+    i = 0
+    if len(lunch) == 0:
+        print("My lunchbox is empty!")
+    else:
+        for food in lunch:
+            if i==0:
+                print("First I eat " + food)
+                i += 1
+            else:
+                print("Next i eat " + food)
+
+eat_lunch(empty_lunch)
+eat_lunch(lunch_box)
