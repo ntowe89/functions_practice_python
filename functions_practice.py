73ba13e9b4e611131bc659271f1f4158841791b8
Nathan Towe <nathanwtowe89@gmail.com>
2024-02-10 14:53:12 -0500

    finished pack function

---
+++ functions_practice.py
@@ -0,0 +1,12 @@
+#2a
+def hello():
+    print("Hello!")
+hello()
+ 
+#2b
+def pack(arg1, arg2, arg3):
+    return [arg1, arg2, arg3]
+print(pack(4,2,8))
+
+empty_lunch = []
+lunch_box = ["PBJ", "chips", "fruitcup"]
